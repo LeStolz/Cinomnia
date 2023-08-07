a0baddc4e6df5e141fcd40c7cd3785381d947fa2
DoanNgocThuc <dnthuc1212@gmail.com>
2023-08-07 09:22:07 +0700

    Update WatchHistory

--- client/src/pages/WatchHistory/WatchHistoryView.tsx
+++ client/src/pages/WatchHistory/WatchHistoryView.tsx
@@ -12,16 +12,21 @@ export function WatchHistoryView() {
 
       <Container>
         <Row>
-          <Col className="col-8">
+          <Col className="order-lg-1 order-sm-2 col-lg-8 col-sm-12">
             <h4 id="type">Watched</h4>
-            <div className="d-flex my-3 bg-secondary rounded" id="film">
-              <Image
-                src="https://ychef.files.bbci.co.uk/976x549/p07h2zhs.jpg"
-                className="w-50 h-auto rounded"
-              />
 
-              <div className="ms-3 pt-2 ">
-                <h3 className="d-flex justify-content-between">Fight Club <i className="bi bi-x-lg me-4" id="delete"></i></h3>
+            <Row className="d-sm-flex justify-content-center">
+              <Col className="d-flex justify-content-center col-5">
+                <Image
+                  src="https://placehold.co/240x150"
+                  className="w-100 h-100 flex-grow-1"
+                />
+              </Col>
+
+              <Col className="pt-2 d-none d-md-block col-7 my-2">
+                <h3 className="d-flex justify-content-between">
+                  Fight Club <i className="bi bi-x-lg me-4" id="delete" />
+                </h3>
                 <h5>David Fincher</h5>
                 <p className="overflow-hidden">
                   A depressed man (Edward Norton) suffering from insomnia meets
@@ -29,58 +34,58 @@ export function WatchHistoryView() {
                   soon finds himself living in his squalid house after his
                   perfect apartment is destroyed.
                 </p>
-              </div>
-            </div>
+              </Col>
+            </Row>
 
-            <div className="d-flex my-3 bg-secondary rounded" id="film">
-              <Image
-                src="https://i.ytimg.com/vi/hbpREOCDCSY/maxresdefault.jpg"
-                className="w-50 h-auto rounded"
-              />
+            <Row className="d-sm-flex justify-content-center my-2">
+              <Col className="d-flex justify-content-center col-5">
+                <Image
+                  src="https://placehold.co/240x150"
+                  className="w-100 h-100 flex-grow-1"
+                />
+              </Col>
 
-              <div className="ms-3 pt-2">
-              <h3 className="d-flex justify-content-between">Blade Runner 2049 <i className="bi bi-x-lg me-4"></i></h3>
+              <Col className="pt-2 d-none d-md-block col-7">
+                <h3 className="d-flex justify-content-between">
+                  Blade Runner 2049{" "}
+                  <i className="bi bi-x-lg me-4" id="delete" />
+                </h3>
                 <h5>Denis Villeneuve</h5>
-                <p>
+                <p className="overflow-hidden">
                   Young Blade Runner K's discovery of a long-buried secret leads
                   him to track down former Blade Runner Rick Deckard, who's been
                   missing for thirty years.
                 </p>
-              </div>
-            </div>
+              </Col>
+            </Row>
           </Col>
 
-          <Col className="col-4">
+          <Col className="order-lg-2 order-sm-1 col-lg-4 col-sm-12">
             <h4>History type</h4>
-              <Form.Group>
-                <hr />
-                <Form.Check
-                  id="watched"
-                  name="type"
-                  label="Watched"
-                  type="radio"
-                />
-                <hr />
-                <Form.Check
-                  id="watching"
-                  name="type"
-                  label="Watching"
-                  type="radio"
-                />
-                <hr />
-                <Form.Check
-                  id="bought"
-                  name="type"
-                  label="Bought"
-                  type="radio"
-                />
-                <hr />
-              </Form.Group>
+            <Form.Group>
+              <hr />
+              <Form.Check
+                id="watched"
+                name="type"
+                label="Watched"
+                type="radio"
+              />
+              <hr />
+              <Form.Check
+                id="watching"
+                name="type"
+                label="Watching"
+                type="radio"
+              />
+              <hr />
+              <Form.Check id="bought" name="type" label="Bought" type="radio" />
+              <hr />
+            </Form.Group>
 
-              <div className="rounded my-3 w-75" id="option">
-                <i className="bi bi-trash me-2" />
-                Clear all watch history
-              </div>
+            <div className="rounded my-3 w-75" id="option">
+              <i className="bi bi-trash me-2" />
+              Clear all watch history
+            </div>
           </Col>
         </Row>
       </Container>
