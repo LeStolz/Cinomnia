ce3b8160fe8bce80b1bed3573bc44131414eb6de
LeStolz <vonamdang03@gmail.com>
2023-08-24 21:18:20 +0700

    Add Auth

--- server/index.ts
+++ server/index.ts
@@ -1,34 +1,31 @@
 import { config } from "dotenv";
+config();
+
 import mongoose from "mongoose";
 import express from "express";
 import cors from "cors";
 import { users } from "./routes/users";
-import { films } from "./routes/film"; 
+import { films } from "./routes/film";
 import { genres } from "./routes/genre";
 import { actors } from "./routes/actor";
 import { directors } from "./routes/director";
 
-config();
-
 mongoose.connect(process.env.DATABASE_URL!);
+const db = mongoose.connection;
+db.on("error", (err) => console.error(err));
+db.once("open", () => console.log("connected to MongoDB"));
 
 const app = express();
 
 app.use(cors({ origin: process.env.CLIENT_URL }));
+app.use(express.json());
 
 app.use("/users", users);
-app.use("/films", films); 
+app.use("/films", films);
 app.use("/genres", genres);
 app.use("/actors", actors);
 app.use("/directors", directors);
 
-
 app.listen(parseInt(process.env.PORT!), () => {
   console.log("listening on port " + parseInt(process.env.PORT!));
 });
-
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-
-
